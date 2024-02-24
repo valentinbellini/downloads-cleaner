164e96f29b2d7f14f4a4f9dbdbb0f31bdf6a7e29
ValentinB <valentinbelliniz@gmail.com>
2024-02-24 17:27:24 -0300

    cleaner.py update - working

--- cleaner.py
+++ cleaner.py
@@ -17,13 +17,24 @@ categories = {
 download_folder = Path.home() / "Downloads"  # Ruta de la carpeta de descargas
 
 def organize_files(folder, categories):
-    count = 0
     for item in os.listdir(folder): # para cada archivo de la carpeta (folder)
         if os.path.isfile(folder / item): # Usamos os.path.isfile() para asegurarnos de que el elemento sea un archivo y no un subdirectorio.
             file_extension = Path(item).suffix.lower()  # Utilizamos el método suffix de la clase Path para obtener la extensión del archivo en minúsculas.
-            count += 1
-            print(file_extension)
-        else: count +=1
-    print(count)
+            destination_folder = None
+            # Iteramos sobre el diccionario categories, que contiene las categorías como claves 
+            # y las extensiones de archivo correspondientes como valores. 
+            for category, extensions in categories.items():
+                if file_extension in extensions:  # Si la extensión del archivo coincide con alguna de las extensiones en una categoría, movemos el archivo a la carpeta de esa categoría.
+                    destination_folder = folder / category
+                    break
+            else:
+                # Si no coincide con ninguna categoría, mover a la carpeta 'Others'
+                destination_folder = folder / "Others"
+                
+            destination_folder.mkdir(exist_ok=True)  # Crear la carpeta si no existe
+            destination_file = destination_folder / item
+            if not destination_file.exists():  # Verificar si el archivo ya existe en la carpeta de destino
+                shutil.move(folder / item, destination_file)
 
+            
 organize_files(download_folder, categories)
